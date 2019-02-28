d895c957c262b8efc0a0205285566165bf8f75e5
David Gouveia <davidluzgouveia@gmail.com>
2019-02-28 13:16:15 +0000

    Added kanjidic script and full data set

---
+++ kanjidic/kanjidic.py
@@ -0,0 +1,71 @@
+import gzip
+import json
+import requests
+import xml.etree.ElementTree as etree
+
+# Converts the KANJIDIC dictionary in xml.gz format into JSON discarding unneeded data
+# The data is downloaded by the script from the official KANJIDIC project website
+# Outputs kanjidic.json with stroke, grade, freq, jlpt, meanings and readings for every kanji
+
+kanji = {}
+
+def get_kanjidic_data():
+
+    url = "http://www.edrdg.org/kanjidic/kanjidic2.xml.gz"
+    request = requests.get(url)
+    data = gzip.decompress(request.content)
+    xml = data.decode("utf-8")
+    tree = etree.fromstring(xml)
+
+    for character in tree.iter("character"):
+        entry = {
+            "strokes": None,
+            "grade": None,
+            "freq": None,
+            "jlpt": None,
+            "meanings": [],
+            "readings_on": [],
+            "readings_kun": []
+        }
+
+        literal = character.find("literal").text
+        kanji[literal] = entry
+
+        misc = character.find("misc")
+        if misc is not None:
+            grade = misc.find("grade")
+            if grade is not None:
+                entry["grade"] = grade.text
+            freq = misc.find("freq")
+            if freq is not None:
+                entry["freq"] = freq.text
+            jlpt = misc.find("jlpt")
+            if jlpt is not None:
+                entry["jlpt"] = jlpt.text
+            stroke_count = misc.find("stroke_count")
+            if stroke_count is not None:
+                entry["strokes"] = stroke_count.text                                   
+        
+        reading_meaning = character.find("reading_meaning")
+        if reading_meaning is not None:
+            rmgroups = reading_meaning.findall("rmgroup")
+            for rmgroup in rmgroups:
+                
+                meanings = rmgroup.findall("meaning")
+                for meaning in meanings:
+                    m_lang = meaning.get("m_lang")
+                    if m_lang is None or m_lang == "en":
+                        entry["meanings"].append(meaning.text)
+                
+                readings = rmgroup.findall("reading")
+                for reading in readings:
+                    r_type = reading.get("r_type")
+                    if r_type == "ja_on":
+                        entry["readings_on"].append(reading.text)
+                    elif r_type == "ja_kun":
+                        entry["readings_kun"].append(reading.text)                    
+
+get_kanjidic_data()
+
+with open("kanjidic.json", "wt", encoding="utf-8") as fp:
+    json.dump(kanji, fp, ensure_ascii=False, indent=4)
