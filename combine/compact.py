25ca3f30e3af77e674610a7bf8ac12f6ebe2360b
David Gouveia <davidluzgouveia@gmail.com>
2019-03-01 18:54:03 +0000

    Improved the compact.py script so that it has automated versions for certain groups of files

--- combine/compact.py
+++ combine/compact.py
@@ -1,14 +1,24 @@
 # Compacts the JSON outputs from kanjidic, wanikani and combine
+# Special input values: kanji, wanikani, kanjidic
 
-filename = input("Enter input filename: ")
+def compact(input_filename, output_filename):
+    with open(input_filename, "rt", encoding="utf-8") as fp:
+        input_data = fp.read()
+    output_data = input_data.replace("\n            ", "")
+    output_data = output_data.replace("\n        ]", "]")    
+    with open(output_filename, "wt", encoding="utf-8") as fp:
+        fp.write(output_data)
 
-with open(filename, "rt", encoding="utf-8") as fp:
-    input_data = fp.read()
+input_filename = input("Enter input filename: ")
 
-output_data = input_data.replace("\n            ", "")
-output_data = output_data.replace("\n        ]", "]")    
-
-filename = input("Enter output filename: ")
-
-with open(filename, "wt", encoding="utf-8") as fp:
-    fp.write(output_data)
+if input_filename == "kanji":
+    compact("..\\kanji.json", "..\\kanji.json")
+    compact("..\\kanji-jouyou.json", "..\\kanji-jouyou.json")
+    compact("..\\kanji-kyouiku.json", "..\\kanji-kyouiku.json")
+elif input_filename == "wanikani":
+    compact("..\\wanikani\wanikani.json", "..\\wanikani\wanikani.json")
+elif input_filename == "kanjidic":
+    compact("..\\kanjidic\kanjidic.json", "..\\kanjidic\kanjidic.json")
+else:
+    output_filename = input("Enter output filename: ")
+    compact(input_filename, output_filename)
