4a50055c3d886ac77d1e5abf4300110be04a2097
David Gouveia <davidluzgouveia@gmail.com>
2019-03-01 18:03:01 +0000

    Refactored JLPT related scripts and data

--- combine/combine.py
+++ combine/combine.py
@@ -1,10 +1,10 @@
 import json
 
-# Takes kanjidic.json and replaces all JLPT values with the ones from jlpt-kanji.json
-# Also adds in information from WaniKani, replacing the readings and meanings when possible
+# Takes kanjidic.json and replaces all JLPT values with the ones from jlpt.json
+# Also adds in information from wanikani.json, replacing the readings and meanings when possible
 
-with open("..\\jlpt\\jlpt-kanji.json", "rt", encoding="utf-8") as fp:
-    jlpt_kanji = json.load(fp)
+with open("..\\jlpt\\jlpt.json", "rt", encoding="utf-8") as fp:
+    jlpt = json.load(fp)
 
 with open("..\\wanikani\\wanikani-kanji.json", "rt", encoding="utf-8") as fp:
     wanikani_kanji = json.load(fp)    
@@ -19,12 +19,12 @@ for key, value in wanikani_kanji.items():
     if key not in kanjidic:
         continue
     kanjidic_value = kanjidic[key]
-    jlpt_value = jlpt_kanji[key] if key in jlpt_kanji else None
+    jlpt_level = jlpt[key] if key in jlpt else None
     entry = {
         "strokes": kanjidic_value["strokes"],
         "grade": kanjidic_value["grade"],
         "freq": kanjidic_value["freq"],
-        "jlpt": jlpt_value,
+        "jlpt": jlpt_level,
         "wanikani": value["level"],
         "radicals": value["radicals"],
         "meanings": value["meanings"],
@@ -37,19 +37,19 @@ for key, value in wanikani_kanji.items():
 for key, value in kanjidic.items():
     if key in output:
         continue
-    jlpt_value = jlpt_kanji[key] if key in jlpt_kanji else None
+    jlpt_level = jlpt[key] if key in jlpt else None
     entry = {
         "strokes": value["strokes"],
         "grade": value["grade"],
         "freq": value["freq"],
-        "jlpt": jlpt_value,
+        "jlpt": jlpt_level,
         "wanikani": None,
         "radicals": None,
         "meanings": value["meanings"],
         "readings_on": value["readings_on"],
         "readings_kun": value["readings_kun"],
     }
-    output[key] = entry    
+    output[key] = entry
 
 with open("kanji.json", "wt", encoding="utf-8") as fp:
     json.dump(output, fp, indent=4, ensure_ascii=False)
--- jlpt/jlpt.py
+++ jlpt/jlpt.py
@@ -4,30 +4,23 @@ import requests
 from lxml import html
 
 # Scrapes JLPT kanji and level information from Jonathan Waller's JLPT Resources page (http://www.tanos.co.uk/jlpt/)
-# Outputs two JSON files, one grouped by level (jlpt-level.json) and another grouped by kanji (jlpt-kanji.json)
+# Outputs a JSON file (jlpt.json) with a mapping of characters to JLPT levels
 
-output_level = {}
-output_kanji = {}
+jlpt = {}
 
-def get_kanji(level):
+def get_jlpt_level(level):
     url = f"http://www.tanos.co.uk/jlpt/jlpt{level}/kanji/"
     page = requests.get(url)
     tree = html.fromstring(page.content)
     cells = tree.xpath('//div[@id="contentright"]/table[2]//tr/td[1]/a')
-    kanjis = []
     for cell in cells:
         kanji = cell.text.strip()
-        output_kanji[kanji] = level
-        kanjis.append(kanji)
-    output_level[level] = kanjis
+        jlpt[kanji] = level
 
 for level in reversed(range(1, 6)):
-    get_kanji(level)
+    get_jlpt_level(level)
     print(f"Fetched data for level {level}")
     time.sleep(2)
 
-with open("jlpt-level.json", "wt", encoding="utf-8") as fp:
-    json.dump(output_level, fp, indent=4, ensure_ascii=False)
-
-with open("jlpt-kanji.json", "wt", encoding="utf-8") as fp:
-    json.dump(output_kanji, fp, indent=4, ensure_ascii=False)
+with open("jlpt.json", "wt", encoding="utf-8") as fp:
+    json.dump(jlpt, fp, indent=4, ensure_ascii=False)
